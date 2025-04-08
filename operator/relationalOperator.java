b1a35949788b4c680d9bcb3abd70d598896a4b9d
vivek singh <vivekthaur3168@gmail.com>
2025-04-08 12:31:53 +0530

    add Relational Operator

---
+++ operator/relationalOperator.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class relationalOperator {
+    
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+    
+            System.out.println("a == b: " + (a == b));  
+            System.out.println("a != b: " + (a != b)); 
+            System.out.println("a > b: " + (a > b));    
+            System.out.println("a < b: " + (a < b));   
+            System.out.println("a >= b: " + (a >= b));  
+            System.out.println("a <= b: " + (a <= b)); 
+        }
+    }
+    
