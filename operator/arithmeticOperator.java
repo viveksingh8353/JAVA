313fa6409500333388483bea2fc4885deb1dde18
vivek singh <vivekthaur3168@gmail.com>
2025-04-08 12:22:22 +0530

    add Arithmetic operator

---
+++ operator/arithmeticOperator.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class arithmeticOperator {
+        public static void main(String[] args) {
+            int a = 15;
+            int b = 4;
+    
+            System.out.println("a + b = " + (a + b));  // Addition
+            System.out.println("a - b = " + (a - b));  // Subtraction
+            System.out.println("a * b = " + (a * b));  // Multiplication
+            System.out.println("a / b = " + (a / b));  // Division (integer division)
+            System.out.println("a % b = " + (a % b));  // Modulus
+        }
+    }
