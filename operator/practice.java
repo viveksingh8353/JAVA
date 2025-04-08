295abfc9c8301a17a96c3257f99b54d57236c0be
vivek singh <vivekthaur3168@gmail.com>
2025-04-08 16:31:40 +0530

    add practice of operator in java

---
+++ operator/practice.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class practice {
+    public static void main(String args[]){
+        int x = 2;
+        int y = 5;
+
+        int exmp1 = (x * y / x);
+        int exmp2 = (x * (y / x));
+
+        System.out.print(exmp1 + " , ");
+        System.out.print(exmp2 + "\n");
+    }
+    
+}
