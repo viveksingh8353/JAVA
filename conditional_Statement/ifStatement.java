182784926f4cc15439d70f4ce5e7f38f48863ac5
vivek singh <vivekthaur3168@gmail.com>
2025-04-09 11:39:29 +0530

    if statement

---
+++ conditional_Statement/ifStatement.java
@@ -0,0 +1,18 @@
+package conditional_Statement;
+import java.util.*;
+
+
+
+public class ifStatement {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the number:");
+    int num = sc.nextInt();
+    
+    if(num > 5){
+        System.out.println("num is greater than 5");
+    }
+    
+}
+}
