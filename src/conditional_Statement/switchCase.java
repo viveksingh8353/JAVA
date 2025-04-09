87e1072cbb69491a614abd3aab1c0839aae7e293
vivek singh <vivekthaur3168@gmail.com>
2025-04-09 18:28:48 +0530

    switch case

---
+++ src/conditional_Statement/switchCase.java
@@ -0,0 +1,36 @@
+package conditional_Statement;
+
+import java.util.Scanner;
+
+public class switchCase {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The number of day: ");
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("satrday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
