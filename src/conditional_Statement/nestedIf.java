effa6cc480cf1b21488a0c1b3418afdcef3bb80b
vivek singh <vivekthaur3168@gmail.com>
2025-04-09 13:10:32 +0530

    add nested if

---
+++ src/conditional_Statement/nestedIf.java
@@ -0,0 +1,12 @@
+package conditional_Statement;
+
+public class nestedIf {
+    public static void main(String args[]){
+        int x = 14;
+        if(x > 5){
+            if(x < 20){
+                System.out.println("x is between 5 and 20");
+            }
+        }
+    }
+}
