d4bec0b1e233b12fe4ff576750d6ea84177cc18e
vivek singh <vivekthaur3168@gmail.com>
2025-04-12 11:58:56 +0530

    Loop

---
+++ src/Loop/evenFrom1to50.java
@@ -0,0 +1,9 @@
+package Loop;
+
+public class evenFrom1to50 {
+    public static void main(String args[]){
+        for(int i = 2; i <= 50; i += 2){
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/Loop/print1to100.java
@@ -0,0 +1,11 @@
+//print numbers from 1 to n
+
+package Loop;
+
+public class print1to100 {
+    public static void main(String args[]){
+        for(int i = 1; i <= 100; i++){
+            System.out.print(i +" ");
+        }
+    }
+}
