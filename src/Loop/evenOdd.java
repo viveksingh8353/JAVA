bd68c3a80f00e96c498c14082bbfa75f83a5446a
vivek singh <vivekthaur3168@gmail.com>
2025-04-18 13:19:52 +0530

    evenOdd 1 to 100

--- src/Loop/evenOdd.java
+++ src/Loop/evenOdd.java
@@ -1,4 +1,13 @@
 package Loop;
 
 public class evenOdd {
+    public static void main(String args[]){
+        for(int i = 1; i <= 100; i++){
+            if(i % 2 == 0){
+                System.out.println(i +" is even");
+            }else{
+                System.out.println(i +" is odd");
+            }
+        }
+    }
 }
