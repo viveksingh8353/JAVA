65783c84fa3ea6e4bf2fe94b6fcc8391a5125263
vivek singh <vivekthakur3168@gmail.com>
2025-04-24 11:56:21 +0530

    Dimond pattern and hollow Pyramid

--- src/pettern/diamondPattern.java
+++ src/pettern/diamondPattern.java
@@ -1,4 +1,26 @@
 package pettern;
 
+//      *
+//    * * *
+//   *  * * *
+//    * * *
+//      *
+
+
 public class diamondPattern {
-}
+    public static void main(String args[]) {
+        int n = 3;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n - i; j++) System.out.print(" ");
+            for(int j = 1; j <= 2*i - 1; j++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i = n-1; i >= 1; i--) {
+            for(int j = 1; j <= n - i; j++) System.out.print(" ");
+            for(int j = 1; j <= 2*i - 1; j++) System.out.print("*");
+            System.out.println();
+        }
+
+    }
+    }
+
